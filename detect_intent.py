45a9bdf1f1f5eacf407d58a1fd9c564ea3791bf9
yk <yefim_korshever@mail.ru>
2023-03-08 17:57:22 +0300

    Setup DialogFlow

---
+++ detect_intent.py
@@ -0,0 +1,69 @@
+from environs import Env
+from google.cloud import dialogflow
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+    session_path = session_client.session_path(project_id, session_id)
+    conversation_items = []
+
+    for text in texts:
+        text_input = dialogflow.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        query_result = session_client.detect_intent(
+            request={"session": session_path, "query_input": query_input}
+        ).query_result
+
+        conversation_items.append(
+            {
+                'query_text': query_result.query_text,
+                'detected_intent': query_result.intent.display_name,
+                'confidence': query_result.intent_detection_confidence,
+                'fulfillment_text': query_result.fulfillment_text,
+            }
+        )
+
+    return {
+        'session_path': session_path,
+        'conversation_items': conversation_items,
+    }
+
+
+def main() -> None:
+    env = Env()
+    env.read_env()
+    user_id = env.str('TELEGRAM_USER_ID')
+    dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
+    texts = [
+        'Кто здесь?',
+        'Хеллоу',
+        'приветствую',
+        'и что?'
+    ]
+
+    conversation_report = detect_intent_texts(
+        project_id=dialogflow_project_id,
+        session_id=user_id,
+        texts=texts,
+        language_code='ru-Ru'
+    )
+
+    print('session path: ', conversation_report['session_path'])
+    for item in conversation_report['conversation_items']:
+        print("=" * 20)
+        print('query text:', item['query_text'])
+        print('detected_intent:', item['detected_intent'])
+        print('confidence:', item['confidence'])
+        print('fulfillment text:', item['fulfillment_text'])
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
 import logging
+import functools
+
 
 from environs import Env
 from telegram import Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, Updater)
 
+from detect_intent import detect_intent_texts
 
 logger = logging.getLogger(__file__)
 
@@ -23,15 +26,31 @@ def help_command(update: Update, context: CallbackContext) -> None:
     )
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def reply(
+    update: Update,
+    context: CallbackContext,
+    project_id: str,
+) -> None:
+    try:
+        message = update.message
+        intent_texts = detect_intent_texts(
+            project_id=project_id,
+            session_id=message.chat_id,
+            texts=[message.text],
+            language_code="ru-Ru",
+        )
+        message.reply_text(
+            intent_texts['conversation_items'][0]['fulfillment_text']
+        )
+    except Exception as error:
+        logger.debug('Reply failed: %s', error)
 
 
 def main() -> None:
     env = Env()
     env.read_env()
     support_bot_token = env('SUPPORT_BOT_TOKEN')
+    dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
 
     logging.basicConfig()
     logger.setLevel(
@@ -45,8 +64,12 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
+    reply_handler = functools.partial(
+        reply,
+        project_id=dialogflow_project_id,
+    )
     dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
+        Filters.text & ~Filters.command, reply_handler))
 
     updater.start_polling()
     updater.idle()
