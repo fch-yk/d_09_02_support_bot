9314cdb6c3cf94ec8ca72e51aa587a3e759606dc
yk <yefim_korshever@mail.ru>
2023-03-10 13:19:19 +0300

    Add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -3,12 +3,20 @@ import random
 import vk_api as vk
 from environs import Env
 from vk_api.longpoll import VkEventType, VkLongPoll
+from detect_intent import detect_intent_texts
 
 
-def echo(event, vk_api):
+def reply(user_id, vk_api, input_text, project_id):
+    intent_texts = detect_intent_texts(
+        project_id=project_id,
+        session_id=user_id,
+        texts=[input_text],
+        language_code="ru-Ru",
+    )
+
     vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
+        user_id=user_id,
+        message=intent_texts['conversation_items'][0]['fulfillment_text'],
         random_id=random.randint(1, 1000)
     )
 
@@ -17,12 +25,13 @@ def main() -> None:
     env = Env()
     env.read_env()
     vk_group_token = env.str('VK_GROUP_TOKEN')
+    dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
     vk_session = vk.VkApi(token=vk_group_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            reply(event.user_id, vk_api, event.text, dialogflow_project_id)
 
 
 if __name__ == '__main__':
